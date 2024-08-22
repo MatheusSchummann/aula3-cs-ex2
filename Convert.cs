276e4200d51677701e0658d41a35140416a4b63a
mathe <mathe@DESKTOP-06LM2TJ>
2024-08-22 16:28:02 -0300

    Adicionar arquivos de projeto.

---
+++ Convert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3_cs_ex2
+{
+    internal class Convert
+    {
+        public List<double> Transferidor(List<string> listaS)
+        {
+            List<double> listaD = new List<double>();
+            listaD = listaS.ConvertAll(double.Parse);
+
+            return listaD; 
+        }
+        public List<double> Calculador(List<double> listaD1)     
+        {
+            List<double> listaD2 = new List<double>();
+
+            listaD2.Add((-listaD1[1] + Math.Sqrt(listaD1[1] * listaD1[1] - 4 * listaD1[0] * listaD1[2])) / (2 * listaD1[0]));
+            listaD2.Add((-listaD1[1] - Math.Sqrt(listaD1[1] * listaD1[1] - 4 * listaD1[0] * listaD1[2])) / (2 * listaD1[0]));
+            
+            return listaD2;
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3_cs_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+        }
+        public Program() 
+        { 
+            Convert converter = new Convert();
+            List<string> listaS = new List<string>();
+            List<double> listaD1 = new List<double>();
+            List<double> listaD2 = new List<double>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Digite o valor 0{0}: ", i + 1);
+                listaS.Add(Console.ReadLine());
+            }
+
+            listaD1 = converter.Transferidor(listaS);
+            listaD2 = converter.Calculador(listaD1);
+
+            foreach (double x in listaD2)
+            {
+                Console.WriteLine("O valor de x foi: {0}",  x);
+            }
+            Console.ReadKey();
+        }
+    
+    
+    }
+}
